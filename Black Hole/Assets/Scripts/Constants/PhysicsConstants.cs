51413cab0048bb73457e017289d1b764d52827b2
Steven Baumann <steven.baumann@mymail.champlain.edu>
2019-11-07 22:04:56 -0500

    Created schwarzschild radius calculation.

--- Black Hole/Assets/Scripts/Constants/PhysicsConstants.cs
+++ Black Hole/Assets/Scripts/Constants/PhysicsConstants.cs
@@ -1,5 +1,8 @@
 ﻿public static class PhysicsConstants
 {
-    // Universal Gravitational Constant
+    // Universal Gravitational Constant (N m2 kg-2)
     public const double G = 6.67430e-11;
+
+    // Speed of light (m/s)
+    public const double c = 299792458;
 }
--- Black Hole/Assets/Scripts/Physics/ModifyRadius.cs
+++ Black Hole/Assets/Scripts/Physics/ModifyRadius.cs
@@ -1,14 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ModifyRadius : MonoBehaviour
 {
-    public float radius;
+    public double mass;
 
     private void Update()
     {
+        float radius = CalculateSchwarzschild(mass);
+
         transform.localScale = new Vector3(radius, radius, radius);
+    }
+
+    private float CalculateSchwarzschild(double mass)
+    {
+        return Convert.ToSingle((2 * PhysicsConstants.G * mass) / (Math.Pow(PhysicsConstants.c, 2)));
     }
 
 }
