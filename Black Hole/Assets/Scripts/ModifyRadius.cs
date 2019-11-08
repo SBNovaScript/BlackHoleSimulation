1d0523e718d7844f85ee0ad0b168d97df82aac96
Steven Baumann <steven.baumann@mymail.champlain.edu>
2019-11-07 21:49:11 -0500

    Initial commit, created scalable radius

---
+++ Black Hole/Assets/Scripts/ModifyRadius.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModifyRadius : MonoBehaviour
+{
+    public float radius;
+
+    private void Update()
+    {
+        transform.localScale = new Vector3(radius, radius, radius);
+    }
+
+}
