fdba7b9ede542cff9c06d57f141f4d3e97df6efa
Steven Baumann <steven.baumann@mymail.champlain.edu>
2020-03-27 09:50:11 -0400

    Scene fixes, added mass

---
+++ BlackHoleProject/Assets/CelestialObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CelestialObject : MonoBehaviour
+{
+
+    public double mass = PhysicsConstants.EARTH_MASS;
+
+}
--- BlackHoleProject/Assets/RotationalDilation.cs
+++ BlackHoleProject/Assets/RotationalDilation.cs
@@ -7,24 +7,51 @@ public class RotationalDilation : MonoBehaviour
     public bool isReference = false;
 
     [Header("Black Hole Settings")]
-    public Transform referenceTransform;
+    public GameObject reference;
 
     void Start()
     {
         if (isReference)
         {
-            StartCoroutine(RotateObject(1, Vector3.up, 1));
-
+            StartCoroutine(RotateObjectStatic(1, Vector3.up, 1));
+        }
+        else
+        {
+            StartCoroutine(RotateObjectFromReference(1, Vector3.up));
         }
     }
 
-    IEnumerator RotateObject(float angle, Vector3 axis, float inTime)
+    IEnumerator RotateObjectStatic(float angle, Vector3 axis, float inTime)
     {
         float rotationSpeed = angle / inTime;
 
         while(true)
         {
             transform.Rotate(axis, rotationSpeed);
+
+            yield return null;
+        }
+    }
+
+    IEnumerator RotateObjectFromReference(float angle, Vector3 axis)
+    {
+
+        double blackHoleMass = reference.GetComponent<CelestialObject>().mass;
+
+        while (true)
+        {
+            float distance = Vector3.Distance(this.transform.position, reference.transform.position);
+            float newDistance = 0.005f + (distance - 0) * (0.1f - 0.005f) / (20f - 0);
+
+            double timeDilation = PhysicsCalculations.getTimeDilationAmount(1.0, blackHoleMass, distance);
+
+            
+
+            float rotationSpeed = (1.0f / (float)timeDilation);
+
+            Debug.Log(rotationSpeed);
+
+            transform.Rotate(axis, 1.0f);
 
             yield return null;
         }
