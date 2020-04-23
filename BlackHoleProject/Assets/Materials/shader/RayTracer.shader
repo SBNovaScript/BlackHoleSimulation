4cb50f337649054bb2322d9ebc4276f69c786c5f
jacksonmichaels <jackson.michaels@mymail.champlain.edu>
2020-04-23 10:50:53 -0400

    more ray tracing

--- BlackHoleProject/Assets/Materials/shader/RayTracer.shader
+++ BlackHoleProject/Assets/Materials/shader/RayTracer.shader
@@ -9,6 +9,7 @@
 		Tags { "RenderType" = "Opaque" }
 		LOD 100
 
+
 		Pass
 		{
 			CGPROGRAM
@@ -27,7 +28,13 @@
 			float4 _TR;
 			float4 _BR;
 			float4 _BHPos;
-			float _Rad;
+			float _BHRad;
+
+			float4x4 _DiskMat;
+
+			int _NumSphereTargets;
+			float4 _SphereTargetsPos[4];
+			float _SphereTargetsRad[4];
 
 
 			struct appdata
@@ -43,25 +50,6 @@
 				float4 vertex : SV_POSITION;
 			};
 
-			//https://gist.github.com/wwwtyro/beecc31d65d1004f5a9d
-			float raySphereIntersect(float3 r0, float3 rd, float3 s0, float sr) {
-				// - r0: ray origin
-				// - rd: normalized ray direction
-				// - s0: sphere center
-				// - sr: sphere radius
-				// - Returns distance from r0 to first intersecion with sphere,
-				//   or -1.0 if no intersection.
-				float a = dot(rd, rd);
-				float3 s0_r0 = r0 - s0;
-				float b = 2.0 * dot(rd, s0_r0);
-				float c = dot(s0_r0, s0_r0) - (sr * sr);
-				if (b*b - 4.0*a*c < 0.0) {
-					return -1.0;
-				}
-				return (-b - sqrt((b*b) - 4.0*a*c)) / (2.0*a);
-			}
-
-
 			v2f vert(appdata v)
 			{
 				v2f o;
@@ -71,16 +59,12 @@
 				UNITY_TRANSFER_FOG(o,o.vertex);
 				return o;
 			}
-
-
-
 		
 			fixed4 frag(v2f i) : SV_Target
 			{
 				int numSteps = 5000;
 				float stepSize = 0.01;
 				float3 bh = _BHPos;
-				bh.z *= -1;
 
 				float4 NormalLeft;
 				float4 NormalRight;
@@ -92,16 +76,40 @@
 				NormalH = normalize(NormalH);
 
 				float3 stepH = NormalH.xyz * stepSize;
+				stepH.z *= -1;
 				float3 pos = float3(0, 0, 0);
 
 				for (int i = 0; i < numSteps; i++)
 				{
 					pos += stepH;
-					if (distance(pos, bh) < _Rad)
+					/*float4 fourPos = float4(pos.x, pos.y, pos.z, 1);
+					float4 newPos = mul(_DiskMat, fourPos);
+
+					if (length(newPos.xz) < 0.00998)
 					{
-						col = float4(1, 1, 1, 1);
+						if (abs(newPos.y < 0.1))
+						{
+							col = float4(1, 1, 1, 1);
+							break;
+						}
+					}*/
+
+					if (distance(pos, _BHPos) < _BHRad)
+					{
+						col = float4(0.5, 0, 0.5, 1);
 						break;
 					}
+
+					
+					for (int j = 0; j < _NumSphereTargets; j++)
+					{
+						if (distance(pos, _SphereTargetsPos[j]) < _SphereTargetsRad[j])
+						{
+							col = float4(1, 1, 1, 1);
+							break;
+						}
+					}
+					
 				}
 
 				return col;
--- BlackHoleProject/Assets/Scripts/Impure/Scene Scripts/CamToShader.cs
+++ BlackHoleProject/Assets/Scripts/Impure/Scene Scripts/CamToShader.cs
@@ -7,20 +7,31 @@ public class CamToShader : MonoBehaviour
 {
     private Material MyMaterial;
     private Shader MyShade;
-
+    public GameObject Disk;
     public GameObject BH;
+    public GameObject[] targets;
     [Range(0, 1)]
     public float rad;
+
+    Camera MyCam;
     // Creates a private material used to the effect
     void Awake()
     {
-        MyShade = Shader.Find("Unlit/RayTracer");
+        MyShade = Shader.Find("Unlit/black_hole_hack");
         MyMaterial = new Material(MyShade);
+
+    }
+
+    private void Start()
+    {
+        MyCam = this.gameObject.GetComponent<Camera>();
+        //MyCam.RenderWithShader(Shader.Find("Unlit/RayTracer"), "ray_tracer");
     }
 
 
     void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
+
         if (!MyMaterial)
         {
             Graphics.Blit(source, destination);
@@ -30,7 +41,20 @@ public class CamToShader : MonoBehaviour
         {
             Vector3[] FrustumCorners = new Vector3[4];
             Vector3[] NormCorners = new Vector3[4];
-            Camera MyCam = this.gameObject.GetComponent<Camera>();
+
+            Vector4[] ptArray;
+            float[] radArray;
+
+            ptArray = new Vector4[targets.Length];
+            radArray = new float[targets.Length];
+
+            MyCam = this.gameObject.GetComponent<Camera>();
+
+            for (int i = 0; i < targets.Length; i++)
+            {
+                ptArray[i] = MyCam.worldToCameraMatrix.MultiplyPoint(targets[i].transform.position);
+                radArray[i] = targets[i].transform.localScale.x;
+            }
 
 
             MyCam.CalculateFrustumCorners(new Rect(0, 0, 1, 1), MyCam.farClipPlane, Camera.MonoOrStereoscopicEye.Mono, FrustumCorners);
@@ -44,9 +68,17 @@ public class CamToShader : MonoBehaviour
             MyMaterial.SetVector("_TR", NormCorners[2]);
             MyMaterial.SetVector("_BR", NormCorners[3]);
 
-            Shader.SetGlobalFloat("_Rad", (MyCam.transform.right * rad).magnitude);
 
             MyMaterial.SetVector("_BHPos", MyCam.worldToCameraMatrix.MultiplyPoint(BH.transform.position));
+            Shader.SetGlobalFloat("_BHRad", (MyCam.transform.right * rad).magnitude);
+
+
+            MyMaterial.SetInt("_NumSphereTargets", ptArray.Length);
+            MyMaterial.SetVectorArray("_SphereTargetsPos", ptArray);
+            MyMaterial.SetFloatArray("_SphereTargetsRad", radArray);
+
+
+            //MyMaterial.SetMatrix("_DiskMat", Disk.transform.worldToLocalMatrix);
 
             Graphics.Blit(source, destination, MyMaterial);
             return;
