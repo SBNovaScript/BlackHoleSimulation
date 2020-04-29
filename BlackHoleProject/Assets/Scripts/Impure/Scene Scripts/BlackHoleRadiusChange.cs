7033e36d4a4b8d47023e4d711c7c3e419f46a9a5
Steven Baumann <steven.baumann@mymail.champlain.edu>
2020-04-29 09:55:48 -0400

    Finished Project.

--- BlackHoleProject/Assets/Scripts/Impure/Scene Scripts/BlackHoleRadiusChange.cs
+++ BlackHoleProject/Assets/Scripts/Impure/Scene Scripts/BlackHoleRadiusChange.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class BlackHoleRadiusChange : MonoBehaviour
 {
-    public float amount;
+    public float amountToScaleBlackHole;
+    
     public CamToShader blackHoleShader;
+    public Transform objectToScale;
+    public float amountToScaleConsoleObject;
 
     void OnSelect()
     {
-        float newAmount = blackHoleShader.rad + amount;
+        float newAmount = blackHoleShader.rad + amountToScaleBlackHole;
         if (newAmount > 0 && newAmount < 1)
         {
-            blackHoleShader.rad += amount;
+            blackHoleShader.rad += amountToScaleBlackHole;
+            objectToScale.localScale = new Vector3(objectToScale.localScale.x + amountToScaleConsoleObject, objectToScale.localScale.y + amountToScaleConsoleObject, objectToScale.localScale.z + amountToScaleConsoleObject);
         }
     }
 }
